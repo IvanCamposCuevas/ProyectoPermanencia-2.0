d56fee73d920886aa200a760708353ae31648e42
Ivan Campos <iva.campos.cuevas@gmail.com>
2018-05-01 19:05:49 -0300

    Se cambio una parte de la clase de conexion porque no satisface las necesidades actuales (se necesita leer mas de una tabla), a comparacion del proyecto que me paso la Marce (que solo utiliza una tabla). Tambien se creo una clase de conexion en negocio para que las demas la usen y se conecten a la Base de Datos.

---
+++ ProyectoPermanencia.DTO/DTOGlobal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPermanencia.DTO
+{
+    public class DTOGlobal
+    {
+        public String Rut { get; set; }
+        public String Nombre { get; set; }
+        public String Jornada { get; set; }
+        public String Escuela { get; set; }
+        public String Carrera { get; set; }
+    }
+}
---
+++ ProyectoPermanencia.Negocio/NegocioConexion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoPermanencia.Conexion;
+namespace ProyectoPermanencia.Negocio
+{
+    public class NegocioConexion
+    {
+        private ConexionBDPermanencia conexionBd;
+
+        public ConexionBDPermanencia ConexionBd
+        {
+            get { return conexionBd; }
+            set { conexionBd = value; }
+        }
+
+        public void ConfigurarConexion() => ConexionBd = new ConexionBDPermanencia
+        {
+            NombreBaseDeDatos = "Permanencia_2",
+            CadenaConexion = "Data Source=(local);Initial Catalog=Permanencia_2;Integrated Security=True"
+        };
+
+    }
+}

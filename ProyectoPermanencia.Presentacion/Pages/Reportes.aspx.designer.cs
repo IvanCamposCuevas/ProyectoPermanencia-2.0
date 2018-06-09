9ea1593e7619d40253cbc601d1b4c45bd3f48a4c
Ivan Campos <iva.campos.cuevas@gmail.com>
2018-06-09 11:54:29 -0400

    ..

--- ProyectoPermanencia.Presentacion/Pages/Reportes.aspx.designer.cs
+++ ProyectoPermanencia.Presentacion/Pages/Reportes.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace ProyectoPermanencia.Presentacion.Pages {
         protected global::System.Web.UI.DataVisualization.Charting.Chart Chart1;
         
         /// <summary>
+        /// Control Chart1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.DataVisualization.Charting.Chart Chart1;
+        
+        /// <summary>
         /// Control SqlDataSource1.
         /// </summary>
         /// <remarks>

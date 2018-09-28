a2625291aeae0fcb202aa5f04785cc9ce43d5e79
Marcela Faúndez <marce.faundezr@gmail.com>
2018-09-28 17:14:01 -0300

    Borre metodos que no se utilizaban ni se iban a utilizar

--- ProyectoPermanencia.Negocio/NegocioPaginaGlobal.cs
+++ ProyectoPermanencia.Negocio/NegocioPaginaGlobal.cs
@@ -126,67 +126,7 @@ namespace ProyectoPermanencia.Negocio
         } // Fin metodo entrega
 
 
-        /**
-         * Ni idea si se usara estas funciones, las comento eso si por si algun día se usan.
-         * */
-
-
-        //public bool validarRut(string rut)
-        //{
-
-        //    bool validacion = false;
-        //    try
-        //    {
-        //        rut = rut.ToUpper();
-        //        rut = rut.Replace(".", "");
-        //        rut = rut.Replace("-", "");
-        //        int rutAux = int.Parse(rut.Substring(0, rut.Length - 1));
-
-        //        char dv = char.Parse(rut.Substring(rut.Length - 1, 1));
-
-        //        int m = 0, s = 1;
-        //        for (; rutAux != 0; rutAux /= 10)
-        //        {
-        //            s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
-        //        }
-        //        if (dv == (char)(s != 0 ? s + 47 : 75))
-        //        {
-        //            validacion = true;
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //    }
-        //    return validacion;
-        //}
-
-        //public string formatearRut(string rut)
-        //{
-        //    int cont = 0;
-        //    string format;
-        //    if (rut.Length == 0)
-        //    {
-        //        return "";
-        //    }
-        //    else
-        //    {
-        //        rut = rut.Replace(".", "");
-        //        rut = rut.Replace("-", "");
-        //        format = "-" + rut.Substring(rut.Length - 1);
-        //        for (int i = rut.Length - 2; i >= 0; i--)
-        //        {
-        //            format = rut.Substring(i, 1) + format;
-        //            cont++;
-        //            if (cont == 3 && i != 0)
-        //            {
-        //                format = "." + format;
-        //                cont = 0;
-        //            }
-        //        }
-        //        return format;
-        //    }
-        //}
-
+        
 
         public DataSet cargarListaCarrera()
         {
